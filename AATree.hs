daea6d6a17a47497d61153e891696ae4a9247487
kevinpettersson <kevin.petterssons@hotmail.com>
2024-05-08 11:33:33 +0200

    Co-authored-by: alicemoss <alicemoss@users.noreply.github.com>

--- AATree.hs
+++ AATree.hs
@@ -24,12 +24,14 @@ data AATree a = Empty | Node {  level :: Int,
                                 left  :: AATree a,
                                 right :: AATree a
                               } deriving (Eq, Show, Read)
-
+-- Returns a empty tree.
+-- Complexity: O(1)
 emptyTree :: AATree a
 emptyTree = Empty
 
 -- Takes a value and checks if the value exists in the tree, if it less than parent value we check in the left sub-tree
 -- otherwise we check in the right sub-tree and if value dosent exist func returns nothing.
+-- Complexity: O(log n)
 get :: Ord a => a -> AATree a -> Maybe a
 get _ Empty = Nothing
 get x (Node _ v l r)
@@ -38,65 +40,55 @@ get x (Node _ v l r)
   | otherwise = Just v
 
 -- Takes a value and creates a node with two empty children.
+-- Complexity: O(1)
 singleton :: a -> AATree a
 singleton x = Node 1 x Empty Empty
 
--- If the tree is empty, create a call singleton function.
--- If value already exist do nothing, return same tree.
--- If value is less than parent value, 
+-- If the tree is empty, call singleton function.
+-- If the insert value is less than root value insert element into left sub-tree, 
+-- and call the skew and split function after inserting to maintain the invariant.
+-- By doing so we recurisvly call insert and check the parent value with the new value, minimizing to a new sub-tree.
+-- Complexity: O(log n)
 insert :: Ord a => a -> AATree a -> AATree a
-insert x Empty             = singleton x
-insert x tree
-  | isJust (get x tree)    = tree                           
+insert x Empty                      = singleton x
 insert x (Node lvl v l r)
-  | x < v                  = split (skew (Node lvl v (insert x l) r))
-  | otherwise              = split (skew (Node lvl v l (insert x r)))
+  | x < v                           = split (skew (Node lvl v (insert x l) r))
+  | x > v                           = split (skew (Node lvl v l (insert x r)))
+  | otherwise                       = Node lvl v l r
 
+-- If parent level is the same as left child level, does a skew.
+-- Complexity: O(1)
 skew :: AATree a -> AATree a
-skew Empty                = Empty
-skew (Node lvl v Empty r) = Node lvl v Empty r
+skew Empty                                = Empty
+skew (Node lvl v Empty r)                 = Node lvl v Empty r
 skew (Node lvl v (Node llvl lv ll lr) r)
-  | llvl == lvl = skew (Node llvl lv ll (Node lvl v lr r)) 
-  | otherwise   =  Node lvl v (Node llvl lv ll lr) r
+  | llvl == lvl                           = Node llvl lv ll (Node lvl v lr r)
+  | otherwise                             =  Node lvl v (Node llvl lv ll lr) r
 
--- You may find it helpful to define
+-- If parent level and right child and right grandchild are on the same level split the 3-node.
+-- Complexity: O(1)
 split :: AATree a -> AATree a
 split Empty                                  = Empty
 split (Node _ _ _ Empty)                     = Empty
-split (Node lvl v l 
-      (Node rlvl rv rl Empty))               = Node lvl v l (Node rlvl rv rl Empty)
-split (Node lvl v l 
-      (Node rlvl rv rl 
-      (Node rrlvl rrv rrl rrr)))
-  | lvl == rlvl && rlvl == rrlvl              = split (Node rlvl rv (Node (lvl+1) v l rl) (Node (rrlvl+1) rrv rrl rrr))
-  | otherwise                                 = Node lvl v l (Node rlvl rv rl (Node rrlvl rrv rrl rrr))
-
-
--- T = Node lvl v r l
--- R = Node rlvl rv rl rr
--- X = Node rrlvl rrv rrl rrr
-
--- After split: 
-
--- R = Node rlvl rv (Node lvl v l rl) (Node rrlvl rrv rrl rrr)    R           T  R  X
---                                                              T   X       A   B 
---                                                            A   B                                                                        
---skew (Node lvl v l (Node rLvl rv rl rr))
-  -- | lvl == rLvl = Node rLvl rv (Node lvl v l rl) rr
-  -- | otherwise = Node lvl v l (Node rLvl rv rl rr)
+split (Node lvl v l(Node rlvl rv rl rr))
+  | lvl == rlvl && rlvl == height rl         = Node rlvl rv (Node lvl v l rl) rr
+  | otherwise                                = Node lvl v l (Node rlvl rv rl rr)
 
 -- Recursivly calls the function on the left and right sub-tree and appending the value in between, creating a list.
+-- Complexity: O(n)
 inorder :: AATree a -> [a]
 inorder Empty = []
 inorder (Node _ v l r) = inorder l ++ [v] ++ inorder r
 
 -- Calculates the total amount of nodes in the tree.
+-- Complexity: O(n)
 size :: AATree a -> Int
 size Empty = 0
 size (Node _ _ l r) = 1 + size l + size r
 
 -- Returns 0 if the tree is empty and otherwise adds 1 and choosing the maximum height of the left and right sub-tree.
 -- By recursively calling the height function.
+-- Complexity: O(n)
 height :: AATree a -> Int
 height tree = case tree of
   Empty -> 0
@@ -110,10 +102,11 @@ remove = error "remove not implemented"
 
 --------------------------------------------------------------------------------
 -- Check that an AA tree is ordered and obeys the AA invariants
-
+-- Complexity: O(n)
 checkTree :: Ord a => AATree a -> Bool
 checkTree tree =
-  isSorted (inorder tree) && all checkLevels (nodes tree)
+  isSorted (inorder tree) 
+  && all checkLevels (nodes tree)
   where
     nodes x
       | isEmpty x = []
@@ -122,39 +115,36 @@ checkTree tree =
 -- True if the given list is ordered
 -- Recursivly checks that left node is less or equal to the right node. And that the level of the left node 
 -- is greater or equal to the right node.
+-- Complexity: O(n)
 isSorted :: Ord a => [a] -> Bool
 isSorted []  = True
 isSorted [x] = True
 isSorted (x:y:xs) = x <= y && isSorted (y:xs)
 
 
--- Check if the invariant is true for a single AA node
--- You may want to write this as a conjunction e.g.
---   checkLevels node =
---     leftChildOK node &&
---     rightChildOK node &&
---     rightGrandchildOK node
--- where each conjunct checks one aspect of the invariant
+-- Checks so the parent lvl is greater than left-child and right child and right grandchild.
+-- Complexity: O(n)
 checkLevels :: AATree a -> Bool
-checkLevels Empty                  = True
-checkLevels (Node _ _ Empty Empty) = True
-checkLevels (Node lvl v l r)       = checkLeft && checkRight && checkLevels l && checkLevels r
-  where
-    checkLeft = case l of
-      Empty                -> True
-      (Node leftLVL _ _ _) -> leftLVL < lvl
-    checkRight = case r of
-      Empty                 -> True
-      (Node rightLVL _ _ _) -> rightLVL <= lvl
+checkLevels Empty             = True
+checkLevels (Node lvl _ l r)
+  | lvl > height l            = True
+  | lvl >= height r           = True
+  | lvl > height (rightSub r) = True
+  | otherwise                 = False
 
+-- Complexity: O(1)
 isEmpty :: AATree a -> Bool
 isEmpty Empty = True
 isEmpty _     = False
 
+-- Returns left subtree
+-- Complexity: O(1)
 leftSub :: AATree a -> AATree a
 leftSub Empty          = Empty
 leftSub (Node _ _ l _) = l
 
+--return right subtree.
+-- Complexity: O(1)
 rightSub :: AATree a -> AATree a
 rightSub Empty          = Empty
 rightSub (Node _ _ _ r) = r
